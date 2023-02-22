b0cc4a214815d68cd688f4a1e8c915e584c2949e
Shawn <shawnpereira97.sp@gmail.com>
2023-02-22 19:40:38 +0530

    Adding Secrets

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import os
+
+try:
+    CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+except KeyError:
+    CLIENT_SECRET = "Token not available!"
+    # or raise an error if it's not available so that the workflow fails
+
 print("Hello World from main.py")
+print(" Secret Value: ".format(CLIENT_SECRET))
