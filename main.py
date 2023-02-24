b20df0606d8467c422e735ef1c20fc47a91e75f1
Shawn <shawnpereira97.sp@gmail.com>
2023-02-24 17:51:29 +0530

    print hello world

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ except KeyError:
     CLIENT_SECRET = "Token not available!"
     # or raise an error if it's not available so that the workflow fails
 
+print(" --- HELLO WORLD ---")
 num = (int)(sys.argv[1])
 if(num%2 == 0):
     print("{} is an even number".format(num))
 else:
     print("{} is an odd number".format(num))
 
-print(" Secret Value: {} ".format(CLIENT_SECRET))
