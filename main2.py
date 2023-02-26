4b2c5f1c0af9a3daaf7bdc679b7301d73cf69cd6
Shawn <shawnpereira97.sp@gmail.com>
2023-02-26 18:42:39 +0530

    error rectification

--- main2.py
+++ main2.py
@@ -2,10 +2,10 @@ import json
 import os
 
 AOBResult = os.environ['GITHUB_OUTPUT']
-print(AOBResult)
+print("value = {}".format(AOBResult))
 
-AOBResultObj = json.loads(AOBResult)
+# AOBResultObj = json.loads(AOBResult)
 
-print("isSucess: {}".format(AOBResultObj['isSuccess']))
-print("jsonString: {}".format(AOBResultObj['jsonString']))
-print("exceptionString: {}".format(AOBResultObj['exceptionString']))
+# print("isSucess: {}".format(AOBResultObj['isSuccess']))
+# print("jsonString: {}".format(AOBResultObj['jsonString']))
+# print("exceptionString: {}".format(AOBResultObj['exceptionString']))
