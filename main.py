e8ab2f652e15ed4849eff4a71564bcc8291ecbcd
Shawn <shawnpereira97.sp@gmail.com>
2023-02-26 17:43:08 +0530

    inputs to script

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import json
 
 try:
     CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+    input_variable = (int)(os.environ['INPUT_STORE'])
 except KeyError:
     CLIENT_SECRET = "Token not available!"
     # or raise an error if it's not available so that the workflow fails
 
-print(" --- HELLO WORLD ---")
+
 AOBResult = "AOBResult"
 AOBResultObj = {
      "isSuccess": True,
@@ -16,12 +17,14 @@ AOBResultObj = {
      "exceptionString": ""
 }
 
+if(input_variable % 2 == 0):
+    print("{} is an EVEN number".format(input_variable))
+else:
+    print("{} is an ODD number".format(input_variable))
+
 if "GITHUB_OUTPUT" in os.environ :
         with open(os.environ["GITHUB_OUTPUT"], "a") as f :
             f.write("AOBResult={}".format(AOBResultObj))
-# num = (int)(sys.argv[1])
-# if(num%2 == 0):
-#     print("{} is an even number".format(num))
-# else:
-#     print("{} is an odd number".format(num))
+
+
 
